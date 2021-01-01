fa28793390f468af051cab32b90e2ef96dbedd1e
cybarspace <sksaad48@gmail.com>
2021-01-01 17:06:20 +0600

    update

--- FooBar.py
+++ FooBar.py
@@ -2,34 +2,6 @@ import copy
 from collections.abc import Iterable
 
 
-class Node:
-    def __init__(self, data, next=None):
-        self.data = data
-        self.next = next
-
-    def __repr__(self):
-        return f"Node object containing < {self.data} >"
-
-    def __str__(self):
-        return str(self.data)
-
-    def __lt__(self, other):
-        assert isinstance(other, Node)
-        return self.data < other.data
-
-    def __le__(self, other):
-        assert isinstance(other, Node)
-        return self.data <= other.data
-
-    def __gt__(self, other):
-        assert isinstance(other, Node)
-        return self.data > other.data
-
-    def __ge__(self, other):
-        assert isinstance(other, Node)
-        return self.data >= other.data
-
-
 class FooBar:
     def __init__(self, data=None, iterable=False):
         """
@@ -259,3 +231,31 @@ class FooBar:
         new = FooBar(sorted(self))
         self.clear()
         self.extend(new)
+
+
+class Node:
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = next
+
+    def __repr__(self):
+        return f"Node object containing < {self.data} >"
+
+    def __str__(self):
+        return str(self.data)
+
+    def __lt__(self, other):
+        assert isinstance(other, Node)
+        return self.data < other.data
+
+    def __le__(self, other):
+        assert isinstance(other, Node)
+        return self.data <= other.data
+
+    def __gt__(self, other):
+        assert isinstance(other, Node)
+        return self.data > other.data
+
+    def __ge__(self, other):
+        assert isinstance(other, Node)
+        return self.data >= other.data
