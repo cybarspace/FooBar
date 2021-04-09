e6930b2517be2a72683835d401a47359b5786b1a
Sheikh <sksaad48@gmail.com>
2021-04-09 18:59:38 +0600

    update

--- foobar.py
+++ foobar.py
@@ -7,15 +7,7 @@ dictionaries while automatically assigning keys
 from dataclasses import dataclass, InitVar
 
 
-def foobar(*args):
-    """ foobar function for dict creation """
-    __baz = {}
-    for val in args:
-        __baz.update({args.index(val): val})
-
-    return __baz
-
-
+# OOP solution for the memes. Use a tuple instead
 @dataclass
 class FooBar:
     """ FooBar class for simple data storage """
@@ -24,7 +16,7 @@ class FooBar:
 
     def __post_init__(self, values):
         try:
-            self.__baz = foobar(*values)
+            self.__baz = (lambda *__baz: dict(enumerate(__baz)))(*values)
         except AttributeError as a_err:
             raise TypeError("Values must be given in list or tuple") from a_err
 
@@ -39,3 +31,22 @@ class FooBar:
             return self.__baz[pos]
         except (KeyError, TypeError) as err:
             raise IndexError("FooBar index is nonexistent") from err
+
+
+# a simple, elegant and effective functional solution
+def foobar(*__baz):
+    """ foobar function for dict creation """
+    return dict(enumerate(__baz))
+
+
+# what I came up with before the above solution:
+# def foobar(*args):
+#    """ foobar function for dict creation """
+#    __baz = {}
+#    for val in args:
+#        __baz.update({args.index(val): val})
+#
+#    return __baz
+
+# for the memes:
+# foobar = lambda *__baz: dict(enumerate(__baz))
